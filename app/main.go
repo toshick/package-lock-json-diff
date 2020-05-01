974bd36d63fc0c855da258fb1cd3f4f68dd267db
toshick <toshick@mercari.com>
2020-05-02 00:55:25 +0900

    feat: diff with interface

--- app/main.go
+++ app/main.go
@@ -5,25 +5,100 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/toshick/package-lock-json-diff/app/model"
 )
 
-func main() {
-	fmt.Println("Hello, 世界")
-	// pkgl := &c{}
-	// fmt.Printf("pkgl %v \n", pkgl)
+var url1 = "../data/before/package-lock.json"
+var url2 = "../data/after/package-lock.json"
 
-	jsonstr := getFile("../data/after/package-lock.json")
+func main() {
+	fmt.Println("\npackage-lock.jsonの差分を検出しよう\n")
+
+	// structによる差分チェック
+	if err := diffJsonStruct(); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// interface{}による差分チェック
+	if err := diffJsonInterface(); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+/*
+ * interface{}による差分チェック
+ */
+func diffJsonInterface() error {
+	json1 := getJsonInterface(url1)
+	json2 := getJsonInterface(url2)
+
+	fmt.Printf("-------- interface diff --------' \n")
+
+	dep1 := json1["dependencies"]
+	dep2 := json2["dependencies"]
+
+	for key := range dep1.(map[string]interface{}) {
+		if diff := cmp.Diff(dep1.(map[string]interface{})[key], dep2.(map[string]interface{})[key]); diff != "" {
+			fmt.Printf("変更あり '%v' \n", key)
+		}
+	}
+	return nil
+}
+
+/*
+ * structによる差分チェック
+ */
+func diffJsonStruct() error {
+	json1 := getJsonStruct(url1)
+	json2 := getJsonStruct(url2)
+
+	fmt.Printf("-------- struct diff --------' \n")
+	for key := range json1.Dependencies {
+		// fmt.Printf("キー %v \n", json1.Dependencies[key])
+		dep1 := json1.Dependencies[key]
+		dep2 := json2.Dependencies[key]
+
+		if diff := cmp.Diff(dep1, dep2); diff != "" {
+			// fmt.Printf(" differs: (%v)\n%s", key, diff)
+			fmt.Printf("変更あり '%v' \n", key)
+		}
+	}
+	return nil
+}
+
+/*
+ * getJsonStruct
+ */
+func getJsonStruct(url string) *model.PackageLockJson {
+	jsonstr := getFile(url)
 	// fmt.Printf("l %v \n", j)
 
 	pglj := new(model.PackageLockJson)
-	err := json.Unmarshal([]byte(jsonstr), pglj)
-	if err != nil {
+
+	if err := json.Unmarshal([]byte(jsonstr), pglj); err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Printf("pglj %v \n", pglj)
-	saveFile(pglj)
+	return pglj
+}
+
+/*
+ * getJsonInterface
+ */
+func getJsonInterface(url string) map[string]interface{} {
+	jsonstr := getFile(url)
+	// fmt.Printf("l %v \n", j)
+
+	pglj := map[string]interface{}{}
+
+	if err := json.Unmarshal([]byte(jsonstr), &pglj); err != nil {
+		log.Fatal(err)
+	}
+	return pglj
 }
 
 /*
@@ -44,13 +119,16 @@ func getFile(url string) string {
 func saveFile(data *model.PackageLockJson) error {
 	bytes, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	// str := string(bytes)
-	// fmt.Println(string)
+	url := "../data/sample.json"
+	if err := ioutil.WriteFile(url, bytes, 0); err != nil {
+		return err
+	}
 
-	if err := ioutil.WriteFile("../data/sample.json", bytes, 0); err != nil {
-		log.Fatalln(err)
+	err = os.Chmod(url, 0777)
+	if err != nil {
+		return err
 	}
 
 	return nil
--- app/model/models.go
+++ app/model/models.go
@@ -1,17 +1,19 @@
 package model
 
+type Dependencies map[string]PackageItem
+
 type PackageItem struct {
-	Version      string                     `json: version`
-	Resolved     string                     `json: resolved`
-	Integrity    string                     `json: integrity`
-	Requires     map[string]string          `json: requires`
-	Dependencies map[string]PackageLockJson `json: dependencies`
-	dev          bool                       `json: dev`
+	Version      string            `json:"version"`
+	Resolved     string            `json:"resolved"`
+	Integrity    string            `json:"integrity"`
+	Requires     map[string]string `json:"requires"`
+	Dependencies Dependencies      `json:"dependencies"`
+	Dev          bool              `json:"dev"`
 }
 
 type PackageLockJson struct {
-	Version         string                 `json: version`
-	LockfileVersion int                    `json: lockfileVersion`
-	requires        bool                   `json: requires`
-	Dependencies    map[string]PackageItem `json: requires`
+	Version         string       `json:"version"`
+	LockfileVersion int          `json:"lockfileVersion"`
+	Requires        bool         `json:"requires"`
+	Dependencies    Dependencies `json:"dependencies"`
 }
